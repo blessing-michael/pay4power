52352e046be46330c256b2c30a8df78d308e1ca3
blessing <blessingmike334@gmail.com>
2024-12-19 14:23:13 +0100

    Add test for empty fields in signup page

--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -1,14 +1,38 @@
 /// <reference types = "cypress" />
 
-it('visit url', ()=>{
-    cy.visit('https://pay4power.com/default.aspx')
-    cy.get('.login-signup > .dropdown-toggle').click()
-   
-    cy.get('a > .btn').click()
-    cy.get('#txtFullname').type('blessing akanimi')
-    cy.get('#txtPhoneNumber').type('07034697295')
-    cy.get('#txtEmail').type('blessingauquo103@gmail.com')
-    cy.get('#chkIAgree').click()
-    cy.get('#btnAdd').click()
+
+describe('Signup page',()=>{
+    beforeEach(()=>{
+        cy.visit('https://pay4power.com/default.aspx')
+        cy.get('.login-signup > .dropdown-toggle').click()
+        cy.get('a > .btn').click()
+
+    })
+    it('visit url', ()=>{
+        
+        cy.get('#txtFullname').type('bless michael ')
+        cy.get('#txtPhoneNumber').type('07034697295')
+        cy.get('#txtEmail').type('asuquoe13@gmail.com')
+        cy.get('#chkIAgree').click()
+        cy.get('#btnAdd').click()
+        
+    })
+    it('should show an error when full name  field is empty', ()=>{
+          // Leave the full name field empty and fill the other fields
+        cy.get('#txtFullname').clear(); 
+        cy.get('#txtPhoneNumber').type('07034697295')
+        cy.get('#txtEmail').type('asuquoe13@gmail.com')
+        cy.get('#chkIAgree').click()
+        cy.get('#btnAdd').click()
+        //Assert that the browser shows an error message
+        cy.get('input[name="txtFullname"]')
+        .should('have.prop', 'validationMessage')
+        .and('not.be.empty');
+ 
     
+    
+    
+    })
+
+
 })
